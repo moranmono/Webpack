c19c836a08e6a08c93cc2d6c287c87207c9dec9b
moranmono <moranm22@gmail.com>
2020-09-16 14:16:25 +0300

    adding sum from dependency

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@
 //import to webpack
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { sum } from './dependency';
 
 //the code
 const root = document.querySelector('#root');
-
-const Component = () => React.createElement('div', null, 123);
+const Component = () => React.createElement('div', null, sum(1,3));
 ReactDOM.render(React.createElement(Component), root);
